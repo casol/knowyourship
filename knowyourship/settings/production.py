2fbfe69809b335a957378c37c5289527345ceac9
casol <casoltys@gmail.com>
2017-07-17 00:00:19 +0200

    Initial commit

---
+++ knowyourship/settings/production.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['knowyourship.com', 'www.knowyourship.com']
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+
+    }
+}
