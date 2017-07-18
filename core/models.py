2d2a1cf13abb79d8d4e99458f26bd18fcc4b7bb0
casol <casoltys@gmail.com>
2017-07-18 20:19:26 +0200

    Create model ListOfMuseumShip

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ListOfMuseumShips(models.Model):
+    """List of museum ships."""
+    ship = models.CharField(max_length=250)
+    country = models.CharField(max_length=250)
+    region = models.CharField(max_length=250)
+    city = models.CharField(max_length=250)
+    from_country = models.CharField(max_length=250)
+    year = models.CharField(max_length=250)
+    ship_class = models.CharField(max_length=250)
+    ship_type = models.CharField(max_length=250)
+    remarks = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.ship
