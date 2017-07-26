fd067eccc72bd5112d234897fd7cb6536c330d91
casol <casoltys@gmail.com>
2017-07-26 23:49:36 +0200

    Add new models

--- core/models.py
+++ core/models.py
@@ -15,3 +15,28 @@ class ShipList(models.Model):
 
     def __str__(self):
         return self.ship
+
+
+class ShipImage(models.Model):
+    """Model responsible for storing images."""
+    title = models.CharField(max_length=200)
+    image_description = models.TextField(blank=True)
+    artist = models.CharField(max_length=200)
+    data = models.CharField()
+    url = models.URLField()
+    slug = models.SlugField(max_length=200, blank=True)
+    usage_terms = models.CharField(max_length=250)
+    license_url = models.URLField()
+    license_short_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+
+class ShipDetails(models.Model):
+    content = models.TextField(blank=True)
+
+
+
+
+
