fc36038f70af861e89c3793dc75ed286977f70c8
casol <casoltys@gmail.com>
2017-08-05 14:39:42 +0200

    Clean up

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.ship_search, name='ship_search'),
-    url(r'^search/$', views.auto_search, name='auto_search'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -2,14 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import ShipList
 from .forms import SearchFrom
-import json
-
-
-def ship_list(request):
-    ships = ShipList.objects.get(id=6)
-    return render(request,
-                  'core/index.html',
-                  {'ships': ships})
 
 
 def ship_search(request):
@@ -28,20 +20,4 @@ def ship_search(request):
                   {'form': form,
                    'results': results,
                    'cd': cd})
-
-
-def auto_search(request):
-    if request.is_ajax():
-        q = request.GET.get('term', '')
-        ships = ShipList.objects.filter(ship__icontains=q)
-        results = []
-        for ship in ships:
-            ship_json = {}
-            ship_json = ship.ship
-            results.append(ship_json)
-        data = json.dumps(results)
-    else:
-        data = 'fail'
-    mimetype = 'application/json'
-    return HttpResponse(data, mimetype)
 
