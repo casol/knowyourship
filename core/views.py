942585ab46739c5f44b6c5260da8735be90c9a94
casol <casoltys@gmail.com>
2017-08-06 23:55:14 +0200

    Add comments

--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ import json
 
 
 def ship_search(request):
+    """Returns query with ship object."""
     form = SearchFrom()
     results = None
     cd = None
@@ -27,6 +28,13 @@ def ship_search(request):
 
 
 def get_ship(request):
+    """jQuery Autocomplete function makes ajax call by itself.
+    When user types a string in autocomplete input field, an AJAX
+    call is made with the ID of the input field to autocomplete
+    function of Jquery-ui. In the source property, a url is supplied
+    which maps to a django view. Then in view we query the model
+    we want to with the parameter named 'term', eg. the term contains
+    “HMS”, a list is generated containing the 'term'."""
     if request.is_ajax():
         q = request.GET.get('term', '')
         ships = ShipList.objects.filter(ship__icontains=q)
