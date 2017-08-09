6c5361881fb1776a3cd4579230a947f844483c76
casol <casoltys@gmail.com>
2017-08-09 19:30:25 +0200

    Add ShipCoordinates model
    Added new model for storing ship coordinates. For simplicity and easy use
    with google maps latitude and longitude is store in FloatField(). Upadeted
    admin site with new model

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import ShipList, ShipDetails
+from .models import ShipList, ShipDetails, ShipCoordinates
 
 
 class ShipDetailsInline(admin.TabularInline):
     """Edit models on the same page as a parent model(ShipList)."""
     model = ShipDetails
+
+
+class ShipCoordinatesInline(admin.StackedInline):
+    """Edit models on the same page as a parent model(ShipList)."""
+    model = ShipCoordinates
 
 
 class ShipListAdmin(admin.ModelAdmin):
@@ -14,6 +19,7 @@ class ShipListAdmin(admin.ModelAdmin):
     search_fields = list_display
     inlines = [
         ShipDetailsInline,
+        ShipCoordinatesInline
     ]
 
 admin.site.register(ShipList, ShipListAdmin)
--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ShipList(models.Model):
-    """List of museum ships."""
+    """Model of museum ships."""
     ship = models.CharField(max_length=250)
     country = models.CharField(max_length=250)
     region = models.CharField(max_length=250, blank=True)
@@ -35,7 +35,7 @@ class ShipImage(models.Model):
 
 
 class ShipDetails(models.Model):
-    """Ship description."""
+    """Ship details description."""
     ship = models.OneToOneField(ShipList, related_name='details')
     content = models.TextField(blank=True)
     remarks = models.CharField(max_length=250, blank=True)
@@ -43,3 +43,15 @@ class ShipDetails(models.Model):
     def __str__(self):
         return self.ship.ship
 
+
+class ShipCoordinates(models.Model):
+    """Ship geographic coordinate, for simplicity purpose model
+    does not use GeoDjango and PostGIS.
+    """
+    ship = models.OneToOneField(ShipList, related_name='coordinates')
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    country = models.CharField(max_length=50)
+
+    def __str__(self):
+        return 'lat: {}, lng: {}'.format(self.latitude, self.longitude)
