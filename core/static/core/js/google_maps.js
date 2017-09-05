b7a2179ed837c3d95db5a57e84c83a95d79971b6
casol <casoltys@gmail.com>
2017-09-05 19:52:46 +0200

    Fix google maps zoom for one location

---
+++ core/static/core/js/google_maps.js
@@ -0,0 +1,46 @@
+function initMap() {
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(51.530616, -0.123125),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+    var markers = new Array();
+
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      markers.push(marker);
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
+
+    function AutoCenter() {
+      //  Create a new viewpoint bound
+      var bounds = new google.maps.LatLngBounds();
+      //  Go through each...
+      $.each(markers, function (index, marker) {
+      bounds.extend(marker.position);
+      });
+      //  Fit these bounds to the map
+      map.fitBounds(bounds);
+    }
+    AutoCenter();
+// zoom for only one location
+    var listener = google.maps.event.addListener(map, "idle", function() {
+      if (map.getZoom() > 16) map.setZoom(16);
+      google.maps.event.removeListener(listener);
+      });
+}
---
+++ core/static/core/js/tests.js
@@ -0,0 +1,34 @@
+function initMap() {
+var markers = [
+    ['Bondi Beach', -33.890542, 151.274856],
+    ['Coogee Beach', -33.923036, 151.259052],
+    ['Cronulla Beach', -34.028249, 151.157507],
+    ['Manly Beach', -33.80010128657071, 151.28747820854187],
+    ['Maroubra Beach', -33.950198, 151.259302]
+];
+}
+    var myOptions = {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false
+    };
+    var map = new google.maps.Map(document.getElementById("map"),myOptions);
+    var infowindow = new google.maps.InfoWindow();
+    var marker, i;
+    var bounds = new google.maps.LatLngBounds();
+
+    for (i = 0; i < markers.length; i++) {
+        var pos = new google.maps.LatLng(markers[i][1], markers[i][2]);
+        bounds.extend(pos);
+        marker = new google.maps.Marker({
+            position: pos,
+            map: map
+        });
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infowindow.setContent(markers[i][0]);
+                infowindow.open(map, marker);
+            }
+        }
+    }(marker, i));
+    map.fitBounds(bounds);
+}
