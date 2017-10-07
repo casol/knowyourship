765c94d1e5be4b8026c9e1ef35f7f9057e263a48
casol <casol@gmail.com>
2017-10-08 00:11:50 +0200

    Create custom search bar
    Added new custom, nice looking search field

--- core/forms.py
+++ core/forms.py
@@ -5,5 +5,6 @@ class SearchForm(forms.Form):
     """Search form."""
     query = forms.CharField(max_length=100,
                             label='',
-                            widget=forms.TextInput(attrs={'placeholder': 'Search'}))
+                            widget=forms.TextInput(attrs={'placeholder': 'Just type ship or country...',
+                                                          'class': 'form-control form-control-lg'}))
 
